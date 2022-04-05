13b18530677defd86c07e368c70d877021e23a11
weygerus <gabrileao38@gmail.com>
2022-04-05 16:45:57 -0300

    05/04/2022-1

--- projeto/src/app/core/navbar/navbar.component.ts
+++ projeto/src/app/core/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { style } from '@angular/animations';
+import { Component, ElementRef, OnInit } from '@angular/core';
+
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -7,47 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   
-  public mostrarmenu: boolean = false
+  public mostrarmenu: boolean = false 
 
-  constructor() {
-
-  }
+  constructor(element: ElementRef) { }
 
   ngOnInit(): void {
   
   }
   
   menuclick(){
+    console.log('ok')
     this.mostrarmenu = !this.mostrarmenu;
-    if(this.mostrarmenu == true){
-      // linha-1
-      document.getElementById('linha1')!.style.transform = 'rotate(45deg)';
-      document.getElementById('linha1')!.style.backgroundColor = '#a106c0'
-      // linha-2
-      document.getElementById('linha2')!.style.opacity = '0';
-      // linha-3
-      document.getElementById('linha3')!.style.transform = 'rotate(-45deg)';
-      document.getElementById('linha3')!.style.marginTop = '-12.5px';
-      document.getElementById('linha3')!.style.backgroundColor = '#a106c0'
-      // mobile-menu
-      document.getElementById('mobile-icon')!.style.paddingTop = '12px';
-      // nav-list
-      document.getElementById('nav-list')!.style.opacity = '1';
-    }
-    else{
-      // linha-1
-      document.getElementById('linha1')!.style.transform = 'rotate(0deg)';
-      document.getElementById('linha1')!.style.backgroundColor = '#101010'
-      // linha-2
-      document.getElementById('linha2')!.style.opacity = '1';
-      // linha-3
-      document.getElementById('linha3')!.style.transform = 'rotate(0deg)';
-      document.getElementById('linha3')!.style.marginTop = '0';
-      document.getElementById('linha3')!.style.backgroundColor = '#101010'
-      // mobile-menu
-      document.getElementById('mobile-icon')!.style.paddingTop = '0';
-      // nav-list
-      document.getElementById('nav-list')!.style.opacity = '0';
-    }
-  }
+        if(this.mostrarmenu == true){
+          // icone
+          document.getElementById('mobile-icon')!.style.transform = 'rotate(-90deg)';
+
+          // desktop
+          document.getElementById('nav-list')!.style.opacity = '1'
+        }
+        else{
+          // icone 
+          document.getElementById('mobile-icon')!.style.transform = 'rotate(0deg)';
+
+          // desktop
+          document.getElementById('nav-list')!.style.opacity = '0'
+        }
+      }
+    
 }
+
+
